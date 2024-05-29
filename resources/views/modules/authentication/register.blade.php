941c302b9a94fda53cf38a4d5da739e51a21ca41
bankareta2 <bankareta2@gmail.com>
2024-05-29 18:49:20 +0700

    check

--- resources/views/modules/authentication/register.blade.php
+++ resources/views/modules/authentication/register.blade.php
@@ -87,12 +87,12 @@
                             </div>
                         </div>
                         <div class="field">
-                            <label for="captcha">Captcha</label>
                             <div>
                                 <img src="{!! captcha_src('flat') !!}" alt="captcha" id="captcha-image">
+                                <br>
                                 <button type="button" id="reload-captcha">Reload</button>
                             </div>
-                            <input type="text" id="captcha" name="captcha">
+                            <input type="text" id="captcha" name="captcha" placeholder="Captcha">
                         </div>
                     </div>
                 </div>
