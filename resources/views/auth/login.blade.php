81511c238599a06fc632668164d6ce9551bab3d6
bankareta2 <bankareta2@gmail.com>
2024-05-29 18:37:38 +0700

    oke

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
                 <div class="field">
                     <label for="captcha">Captcha</label>
                     <div>
-                        <img src="{{ captcha_src('flat') }}" alt="captcha" id="captcha-image">
+                        <img src="{!! captcha_src('flat') !!}" alt="captcha" id="captcha-image">
                         <button type="button" id="reload-captcha">Reload</button>
                     </div>
                     <input type="text" id="captcha" name="captcha">
